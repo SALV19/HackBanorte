0c5c70efac1d2aaa72f7dcb5ee074d73dbfbae9e
SALV <a01707122@tec.mx>
2026-09-12 12:56:32 -0600

    feat: db schemas

---
+++ backend/model/transactions.model.ts
@@ -0,0 +1,41 @@
+import { InferSchemaType, Types } from "mongoose";
+import { model, Schema } from "mongoose";
+
+const TransactionsSchema = new Schema(
+  {
+    id_user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    reason: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+export type Transactions = InferSchemaType<typeof TransactionsSchema>;
+
+// MODEL
+const TransactionsModel = model<Transactions>(
+  "Transactions",
+  TransactionsSchema,
+);
+
+export type CreateTransactionDTO = Omit<Transactions, "_id" | "updatedAt"> & {
+  createdAt?: Date;
+};
+
+export const TransactionsDataAccess = {
+  getMonthlyExpenses: async (userId: string) => {
+    return await TransactionsModel.find({ id_user: userId }).exec();
+  },
+
+  createTransaction: async (transactionData: CreateTransactionDTO) => {
+    return await TransactionsModel.create({
+      ...transactionData,
+      createdAt: transactionData.createdAt ?? new Date(),
+    });
+  },
+};
