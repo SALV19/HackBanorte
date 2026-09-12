98b767aa5499ec5619e4d216dd070a81d396c4f6
Yessica <a01273574@tec.mx>
2026-09-12 17:08:13 -0600

    [ADD] tokens

--- backend/model/PreguntaCache.ts
+++ backend/model/PreguntaCache.ts
@@ -3,7 +3,6 @@ import { Schema, model } from "mongoose";
 const preguntaCacheSchema = new Schema(
   {
     pregunta: { type: String, required: true },
-    embedding: { type: [Number], required: true },
     componentes: { type: Schema.Types.Mixed, required: true },
   },
   { timestamps: true },
